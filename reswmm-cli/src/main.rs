edc07bb55530a896e92e842f840b9feb17b97099
Reese Walton <jrwalt4@gmail.com>
2021-02-17 20:04:28 -0500

    use clap argument parser

--- reswmm-cli/src/main.rs
+++ reswmm-cli/src/main.rs
@@ -1,20 +1,28 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
 use reswmm_core::{
     units::{qnty::Qnty, system::si::Length},
     xsection,
 };
 
-#[allow(unused)]
-macro_rules! my_macro {
-    ($var:block) => {
-        println!("result of '{}' is '{}'", stringify!($var), $var);
-    };
-}
-
 fn main() {
-    let width = Qnty::<Length>::new(2.0);
-    let depth = Qnty::<Length>::new(2.0);
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let width_arg = matches.value_of("width").unwrap(); // required input
+    let width = Qnty::<Length>::new(width_arg.parse().expect("Invalid width"));
+
+    let depth_arg = matches.value_of("depth").unwrap(); // required input
+    let depth = Qnty::<Length>::new(depth_arg.parse().expect("Invalid depth"));
+
     let rect = xsection::new_xs(xsection::Kind::Rectangle, width);
-    println!("Rect area at {:?} is {:?}.", depth, rect.area(depth));
-    let hello = "world";
-    my_macro![{ hello }];
+    let area = rect.area(depth);
+
+    let verbose = matches.is_present("verbose");
+    if verbose {
+        println!("Rect area at {:?} is {:?}.", depth, area);
+    } else {
+        println!("{:?}", area);
+    }
 }
