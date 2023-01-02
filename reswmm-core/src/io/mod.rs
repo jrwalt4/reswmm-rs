9c8637f3360e85e52c036b600bbf15e5a4d7a284
Reese Walton <jrwalt4@gmail.com>
2023-01-02 11:50:58 -0500

    add serde for Regions

--- reswmm-core/src/io/mod.rs
+++ reswmm-core/src/io/mod.rs
@@ -3,16 +3,25 @@
 #[cfg(test)]
 mod io_test {
     use crate::node::{NodeElement, Junction};
+    use crate::region::{RegionElement, SubBasin};
     use serde_json;
+
+    macro_rules! test_ser_de {
+        ($elem:expr) => {
+            let elem = $elem;
+            let elem_serialized = serde_json::to_string(&elem).unwrap();
+            let elem_deserialized = serde_json::from_str(&elem_serialized).unwrap();
+            assert_eq!(elem, elem_deserialized);
+        };
+    }
 
     #[test]
     fn serde_node() {
-        let n1 = NodeElement::new(1, "N1", Junction::new(101.0));
-        let n1_json = serde_json::to_string(&n1).unwrap();
-        
-        let n1_de: NodeElement = serde_json::from_str(&n1_json).unwrap();
-        assert_eq!(n1, n1_de);
-        
+        test_ser_de!(NodeElement::new(1, "N1", Junction::new(101.0)));
+    }
+
+    #[test]
+    fn serde_node_json() {
         let n2 = NodeElement::new(2, "N2", Junction::new(102.0));
         let n2_json = serde_json::json!({
             "kind": "Junction",
@@ -22,5 +31,10 @@ mod io_test {
         }).to_string();
         let n2_de = serde_json::from_str(&n2_json).unwrap();
         assert_eq!(n2, n2_de);
+    }
+
+    #[test]
+    fn serde_region() {
+        test_ser_de!(RegionElement::new(1, "SB1", SubBasin::new(25.0)));
     }
 }
