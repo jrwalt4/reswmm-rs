a21b3f116aac3c39f417cef3e796a35571911564
Reese Walton <jrwalt4@gmail.com>
2023-04-15 12:03:14 -0400

    Project::add_router

--- reswmm-core/src/project.rs
+++ reswmm-core/src/project.rs
@@ -1,13 +1,75 @@
-/// Project container for nodes, links, regions, etc.
 
-use bevy_ecs::prelude::*;
+use crate::router::Router;
 
+use bevy_ecs::{
+    component::ComponentId,
+    prelude::*
+};
+
+use std::collections::HashMap;
+
+/// Container for elements and routers that makeup a simulation.
+/// Functions like a [`App`](bevy_app::app::App).
+/// 
+/// # Examples
+/// ```
+/// # use bevy_ecs::prelude::*;
+/// # use reswmm_core::project::Project;
+/// #[derive(Component, Debug)]
+/// struct Flow(f32);
+/// 
+/// fn dummy_router(mut flows: Query<(Entity, &Flow)>) {
+///     for (id, Flow(q)) in &flows {
+///         println!("Flow for element '{id:?}' = {q}");
+///     }
+/// }
+/// 
+/// let mut prj = Project::new();
+/// prj.add_router(dummy_router);
+/// ```
+#[derive(Default)]
 pub struct Project {
-    model: World
+    model: World,
+    schedule: Schedule,
+    graph: HashMap<ComponentId, Vec<ComponentId>>,
 }
 
 impl Project {
+    /// Create a new Project using [`default`](std::default::Default::default).
     pub fn new() -> Self {
-        Project { model: World::new() }
+        Default::default()
+    }
+
+    /// Add a [`Router`] to the [`Project`].
+    pub fn add_router<M, R: Router<M>>(&mut self, router: R) -> &mut Self {
+        let mut router = router.into_system();
+        {
+            router.initialize(&mut self.model);
+        }
+        let access = router.component_access();
+        for dep in access.reads() {
+            self.graph.entry(dep)
+                .and_modify(|e| { e.push(dep)})
+                .or_insert(vec![dep]);
+        }
+        for id in access.writes() {
+            use std::collections::hash_map::Entry::*;
+            match self.graph.entry(id) {
+                Occupied(_) => {
+                    let dup = self.model.components().get_name(id).unwrap_or("{unknown}");
+                    panic!("Multiple ownership for component {}", dup);
+                },
+                Vacant(e) => {
+                    e.insert(Vec::new());
+                }                
+            }
+        }
+        self.schedule.add_system(router);
+        self
+    }
+
+    /// Run this [Project].
+    pub fn run(&mut self) {
+        self.schedule.run(&mut self.model);
     }
 }
--- reswmm-core/src/router.rs
+++ reswmm-core/src/router.rs
@@ -7,7 +7,26 @@ use crate::element::UID;
 
 use std::collections::HashMap;
 
-use bevy_ecs::prelude::*;
+use bevy_ecs::{
+    prelude::*,
+    system::IntoSystem
+};
+
+/// A router is really just a [bevy System](bevy_ecs::system::System) with an extra 
+/// constraint that only one router can be responsible for writing to the [`Next`] 
+/// value of a parameter component. Other changes to the parameter can be
+/// [`deferred`](bevy_ecs::system::Deferred) to after the end of a step, but only one router
+/// can be responsible for calculating the value. This relationship is used to schedule the routers
+/// so that subsequent routers that depend on the output value can use it as an input. 
+pub trait Router<M>: IntoSystem<(), (), M> + IntoSystemConfig<M> {
+
+    /// Convenience for turning functions into [System](bevy_ecs::system::System)'s
+    fn into_system(self) -> <Self as IntoSystem<(), (), M>>::System {
+        IntoSystem::into_system(self)
+    }
+}
+
+impl<M, F: IntoSystem<(), (), M> + IntoSystemConfig<M>> Router<M> for F {}
 
 #[derive(Resource, Default)]
 pub struct Nodes(HashMap<Entity, UID>);
