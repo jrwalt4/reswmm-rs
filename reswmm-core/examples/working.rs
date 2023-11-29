500ae8fb5168a87b3e4a39d84fade4e31a0f6883
Replit user <>
2023-11-29 14:56:31 +0000

    Use bevy_app inside Project

--- reswmm-core/examples/working.rs
+++ reswmm-core/examples/working.rs
@@ -1,9 +1,6 @@
-use reswmm_core::{
-    project::Project,
-    router::*,
-    time::Clock
-};
+use reswmm_core::{element::*, project::Project, router::*, time::Clock};
 
+use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
 
 #[derive(Component)]
@@ -12,16 +9,29 @@ struct Length(f32);
 #[derive(Component, Clone, Debug)]
 struct Depth(f32);
 
+fn spawn_entities(mut commands: Commands) {
+    commands.spawn_batch(vec![
+        (Element::new(1, "J1"), Length(10.0), Depth(1.0)),
+        (Element::new(2, "J2"), Length(5.0), Depth(4.0)),
+    ]);
+}
+
 fn depth_router1(mut query: Query<(&Length, &Depth, &mut Next<Depth>)>, time: Res<Clock>) {
-    eprintln!("1 - Step: {:?}, Time: {:?}", time.step_count, time.simulation);
-    for (Length(l), prev,  mut next) in &mut query {
+    eprintln!(
+        "1 - Step: {:?}, Time: {:?}",
+        time.step_count, time.simulation
+    );
+    for (Length(l), prev, mut next) in &mut query {
         eprintln!("1 - Length: {l}, Prev: {prev:?}, Next: {:?}", **next);
         **next = Depth(prev.0 + l * 0.1);
     }
 }
 
 fn depth_router2(query: Query<(&Length, &Depth, &Next<Depth>)>, time: Res<Clock>) {
-    eprintln!("2 - Step: {:?}, Time: {:?}", time.step_count, time.simulation);
+    eprintln!(
+        "2 - Step: {:?}, Time: {:?}",
+        time.step_count, time.simulation
+    );
     for (Length(l), prev, next) in &query {
         eprintln!("2 - Length: {l}, Prev: {prev:?}, Next: {:?}", *next);
     }
@@ -29,14 +39,12 @@ fn depth_router2(query: Query<(&Length, &Depth, &Next<Depth>)>, time: Res<Clock>
 
 fn main() {
     let mut prj = Project::new();
-    prj.add_element(1, "J1", (Length(10.0), (Depth(1.0))));
-    prj.add_element(2, "J2", (Length(10.0), (Depth(4.0))));
-    prj.register_variable::<Depth>();
-
-    prj.add_router(depth_router1).add_system(depth_router2.after(depth_router1));
+    prj.add_system(PreStartup, spawn_entities)
+        .register_variable::<Depth>()
+        .add_router(depth_router1)
+        .add_router(depth_router2);
     //prj.add_router(depth_router2);
-    prj.run();
-    prj.run();
+    prj.run_one();
+    prj.run_one();
     //prj.run();
-
 }
--- reswmm-core/src/project.rs
+++ reswmm-core/src/project.rs
@@ -1,90 +1,73 @@
-
 use crate::{
     element::{Element, UID},
-    router::{InitSchedule, PreStepSet, StepSet, PostStepSet, Variable, variable_advance_system, variable_init_next},
-    time::{Clock, ClockSettings, StepRequest, clock_controller}
+    router::{
+        variable_advance_system, variable_init_next, InitSchedule, PostStepSet, PreStepSet,
+        StepSet, Variable,
+    },
+    time::{clock_controller, Clock, ClockSettings, StepRequest},
 };
-
-use bevy_ecs::{
-    prelude::*,
-    world::EntityMut
-};
+use bevy_app::prelude::*;
+use bevy_ecs::prelude::*;
+use bevy_ecs::schedule::ScheduleLabel;
 
 /// Container for elements and routers that makeup a simulation.
 /// Functions like a [`App`](bevy_app::app::App).
-/// 
+///
 /// # Examples
 /// ```
 /// # use bevy_ecs::prelude::*;
 /// # use reswmm_core::project::Project;
 /// #[derive(Component, Debug)]
 /// struct Flow(f32);
-/// 
+///
 /// fn dummy_router(mut flows: Query<(Entity, &Flow)>) {
 ///     for (id, Flow(q)) in &flows {
 ///         println!("Flow for element '{id:?}' = {q}");
 ///     }
 /// }
-/// 
+///
 /// let mut prj = Project::new();
 /// prj.add_router(dummy_router);
 /// ```
-#[derive(Default)]
 pub struct Project {
-    model: World,
-    schedule: Schedule,
+    app: App,
     //graph: HashMap<ComponentId, Vec<ComponentId>>,
-    init_schedule: Option<Schedule>,
-    step_schedule: Schedule,
 }
 
 impl Project {
     /// Create a project with initialization for typical use
     pub fn new() -> Self {
-        let mut prj = Self::default();
-        prj.schedule
-            .configure_sets(PreStepSet)
-            .configure_sets(StepSet.after(PreStepSet))
-            .configure_sets(PostStepSet.after(StepSet))
-            .add_systems(clock_controller.in_set(PostStepSet));
-        prj.model.init_resource::<Clock>();
-        prj.model.init_resource::<ClockSettings>();
-        prj.model.init_resource::<Events<StepRequest>>();
-        prj
-    }
-
-    /// Add an element to the model. Elements are [bevy Entities](bevy_ecs::entity::Entity) with a 
-    /// [`UID`] and [`Name`](crate::element::Name) component. 
-    /// 
-    /// # Examples
-    /// ```
-    /// # use reswmm_core::{element::Name, project::Project};
-    /// # use bevy_ecs::prelude::*;
-    /// let mut prj = Project::new();
-    /// let el = prj.add_element(1, "J1", ());
-    /// 
-    /// fn read_one(elements: Query<(Entity, &Name)>) {
-    ///     let (_id, Name(name)) = elements.single();
-    ///     assert_eq!(name, "J1");
-    /// }
-    /// 
-    /// prj.add_router(read_one).run();
-    /// 
-    /// ```
-    pub fn add_element<I: Into<UID>, S:ToString, P: Bundle>(&mut self, uid: I, name: S, props: P) -> EntityMut {
-        self.model.spawn((Element::new(uid, name), props)).into()
+        let mut app = App::default();
+        app.configure_sets(
+            Update,
+            (
+                PreStepSet,
+                StepSet.after(PreStepSet),
+                PostStepSet.after(StepSet),
+            ),
+        )
+        .add_systems(PostUpdate, clock_controller)
+        .init_resource::<Clock>()
+        .init_resource::<ClockSettings>()
+        .init_resource::<Events<StepRequest>>();
+        Self { app }
     }
 
     pub fn register_variable<T: Variable>(&mut self) -> &mut Self {
-
-        self.init_schedule.get_or_insert_with(|| Schedule::new(InitSchedule)).add_systems(IntoSystem::into_system(variable_init_next::<T>));
-        self.step_schedule.add_systems(IntoSystem::into_system(variable_advance_system::<T>));
+        self.app.add_systems(
+            PostStartup,
+            IntoSystem::into_system(variable_init_next::<T>),
+        );
+        self.app.add_systems(
+            PostUpdate,
+            IntoSystem::into_system(variable_advance_system::<T>),
+        );
         self
     }
 
     /// Add a [`Router`] to the [`Project`].
     pub fn add_router<M, R: IntoSystem<(), (), M>>(&mut self, router: R) -> &mut Self {
-        /* 
+        /*
         let mut router = IntoSystem::into_system(router);
         {
             router.initialize(&mut self.model);
@@ -104,28 +87,31 @@ impl Project {
                 },
                 Vacant(e) => {
                     e.insert(Vec::new());
-                }                
+                }
             }
         }
         // */
-        self.schedule.add_systems(router.in_set(StepSet));
+        self.app.add_systems(Update, router.in_set(StepSet));
         self
     }
 
-    pub fn add_system<M, S: IntoSystemConfigs<M>>(&mut self, system: S) -> &mut Self {
-        self.schedule.add_systems(system);
+    /// Add system directly, which can be added to any schedule
+    /// (Startup, Update, PostUpdate, etc.).
+    pub fn add_system<M>(
+        &mut self,
+        schedule: impl ScheduleLabel,
+        system: impl IntoSystemConfigs<M>,
+    ) -> &mut Self {
+        self.app.add_systems(schedule, system);
         self
     }
 
-    /// Run this [Project].
-    pub fn run(&mut self) {
-        // only run the init_schedule once, leave None in its place. 
-        if let Some(mut init) = self.init_schedule.take() {
-            self.schedule.initialize(&mut self.model).unwrap();
-            init.run(&mut self.model);
-            init.apply_deferred(&mut self.model);
-        }
-        self.schedule.run(&mut self.model);
-        self.step_schedule.run(&mut self.model);
+    /// Run this [`Project`].
+    pub fn run_to_end(&mut self) {
+        self.app.run();
+    }
+
+    pub fn run_one(&mut self) {
+        self.app.update();
     }
 }
